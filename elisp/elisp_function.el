676d58bd68342abcdda5b949addd6761890757f7
JanSky520 <1102812928@qq.com>
2024-08-29 23:50:23 +0800

    完成第二章

---
+++ elisp/elisp_function.el
@@ -0,0 +1,12 @@
+(buffer-name)        ;显示当前buff名字
+(buffer-file-name)   ;显示当前buff完整路径
+(current-buffer)     ;显示当前buff
+(other-buffer)       ;显示前一个buff的名字
+(switch-to-buffer (other-buffer))  ;跳转到上一个buffer
+(buffer-size)        ;显示当前缓冲区的大小
+(point)              ;表示从缓冲区开头到光标所在位置的字符数量
+(point-min)          ;返回当前缓冲区中 point 的最小允许值
+(point-max)          ;返回当前缓冲区中 point 的最大允许值
+
+
+
